b8e358fb7bd4e2acb7a90c667006f8a92c31e986
Andrew Gibb <andrew.gibb@bbc.co.uk>
2018-01-09 15:17:39 +0000

    Migrated to homebrew bundles for mac cli apps

--- maccli.sh
+++ maccli.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
 
-CLIPROGS="gcc pkg-config ghostscript imagemagick tmux netpbm homebrew/science/hdf5 htop-osx ilmbase openexr cmake"
-
-# Possible others:
-# protobuf241
-# python
-# cmake
-# freetype
-# glew
-# gnuplot
-# graphicsmagick
-# libtool
-# osctave
-
-for prog in `echo $CLIPROGS`
-do
-  brew install $prog
-done
-
-exit 0
+# Installs the homebrew configuration as specified in Brewfile
+brew bundle
